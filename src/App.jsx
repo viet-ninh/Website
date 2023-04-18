7a2166e346a56d2739f24e504bdb922d7d2cba5d
vietninh <vietquocninh@gmail.com>
2023-04-18 11:25:32 -0500

    Adding custom styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from '@material-ui/core';
 
+import useStyles from './css/App';
 /*
 To change the icon look at material-ui's icons
 */
 import { Face } from '@material-ui/icons';
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <>
       <CssBaseline/>
-        <AppBar position="relative">
+        <AppBar position="relative" className={classes.navbar}>
           <Toolbar>
                 <Face />
                 <Typography variant = "h6">
@@ -19,12 +22,32 @@ const App = () => {
               </Toolbar>
         </AppBar>
       <main>
-        <div>
+        <div className='classes.container'>
           <Container maxWidth="sm">
-            <Typography variant="h5" align="center" color="textPrimary" paragraph>
-              Hello! Welcome to Viet Ninh's Website.
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+              Viet Ninh
             </Typography>
+            <Typography variant="h6" align="center" color="textSecondary" paragraph>
+              Hello, welcome to my website!
+            </Typography>
+            <div>
+
+              <Grid container spacing={2} justifyContent='center'>
+                <Grid item>
+                  <Button variant="contained">
+                    Call Me
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Email Me
+                  </Button>
+                </Grid>
+              </Grid>
+
+            </div>
           </Container>
+
         </div>
       </main>
     </>
---
+++ src/css/App.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  navbar:{
+    background: "#0A2463",
+    marginBottom: "50px",
+    color: "#FFFC7F7"
+  },
+  primaryButton:{
+    background: "#2C2A4A",
+    color: "#FFFC7F7"
+  }
+}));
+
+export default useStyles;
