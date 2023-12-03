dd7162d217a55c4b0c4817f184e9facb1256a559
vietninh <vietquocninh@gmail.com>
2023-12-03 15:46:36 -0600

    Updates

---
+++ precache-manifest.75a727127c52401333df1e6b417fcf4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f693e0e05a9cd7b7ac8774e6c3866503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9c3f3dbf6b91ee5b0c3",
+    "url": "/static/js/2.90034a03.chunk.js"
+  },
+  {
+    "revision": "57025f8e73cb557d93be117a65fed0e2",
+    "url": "/static/js/2.90034a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "574c1df7fed156cc777e",
+    "url": "/static/js/main.70c78ae0.chunk.js"
+  },
+  {
+    "revision": "c9935929a832abc79d84",
+    "url": "/static/js/runtime-main.a8c2270f.js"
+  },
+  {
+    "revision": "1e65a178041b59d3b45d342c98e9b948",
+    "url": "/static/media/VSA_Food_GBM.1e65a178.JPG"
+  },
+  {
+    "revision": "c6fd010a5ab52d838fbdd1ad4d75bbfd",
+    "url": "/static/media/profile.c6fd010a.jpg"
+  }
+]);
