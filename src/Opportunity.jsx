f7568ed20dedcce82066f8d7731cb1378abed7ae
vietninh <vietquocninh@gmail.com>
2023-12-13 01:00:07 -0600

    Updated research and opportunity pages

--- src/Opportunity.jsx
+++ src/Opportunity.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import { Typography, Container } from '@material-ui/core';
-
+import building from './static/research_images/3D-printed-building-Dubai.jpg'
 const Opportunity = () => {
   return(
     <Container>
       <Typography variant="h3" align="center" gutterBottom>
         My Research Opportunity
+      </Typography>
+      <Typography variant="h6" color="textSecondary" paragraph>
+        At the beginning of my senior year, I was determined of going into graduate school. I never really thought about going into graduate
+        until the summer after my junior year in 2023. This was mainly due to my school, the University of Nebraska Lincoln, establishing the
+        accelerated program for software engineering students to receive a masters degree in computer science with only one additional year after
+        I graduate with my masters. This was an opportunity that I believed to be extremely valuable, not only for my career, but for myself as 
+        a student as well because, while I am still young, I will have the capacity to continue on learning at an accelerated rate.
+      </Typography>
+      <div style={{ textAlign:"center" }}>
+        <img src={building} align="center" height="250" alt="3D printed building in Dubai"/>
+        <Typography>
+          This image showcases an image of a building that was 3D printed. This building is located in Dubai, and it is the biggest 3D printed
+          building in the world!
+        </Typography>
+        <br/>
+      </div>
+      <Typography variant="h6" color="textSecondary" paragraph>
+        To get started on getting an opportunity with graduate school, I decided to do research for the university to get familiar with how
+        graduate research would be like. I contacted Dr. Sris-an from the School of Computing on whether he was in charge of any research projects
+        and luckily enough he did, and that he was looking for undergraduate to help him on the tasks. Dr. Srisa-an's research team is working on a
+        solution to detect malicious or bad 3D model files. What causes is this problem is that, malicious people on the internet go around and inject
+        what is known as voids or hollow spaces inside of 3D models that would be used for 3D printing. What those spaces inside of the models cause is
+        that the object, when 3D printed, becomes fragile and its durability gets worse. This is a problem because as 3D printing is becoming
+        more important it becomes essential to standard users to know that the file that they will be handling is safe.
+      </Typography>
+      <Typography variant="h6" color="textSecondary" paragraph>
+        My task for this project is to collect 3D models from the internet to find 3D models with voids or hollow spaces inside of the model.
+        The goal of this task is to be able to find 3D models whose voids are intentional and that they are good for the model. With the data from the models,
+        we will be able to create a program that would be able to determine whether a model with a void or space inside of it is intentional or not.
       </Typography>
     </Container>
   );
--- src/Research.jsx
+++ src/Research.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import { Typography, Container } from '@material-ui/core';
 import thingiverse_image from './static/research_images/thingiverse_image.png'
-import model_image from './static/research_images/prusaslicer-bird.jpg'
+import model_image from './static/research_images/prusaslicer-base.JPG'
 const VSA = () =>{
   return (
     <Container maxWidth="med">
       <Typography variant="h3" align="center" color="textPrimary" gutterBottom>
-          My Research Experience
+        My Research Experience
         </Typography>
-      <Typography variant="h6" color="textSecondary" paragraph>
-        In my undergraduate career, I always wondered what students would research. In the beginning of my senior year, I was given a chance to do
-        research under Dr. Srisa-an. I have had the privilege to be able to learn from the professors and other grad students that I had the opportunity
-        to work with. Although, the work is remote and involves me sitting in front of a computer the whole time, it has still provided me a lot of chances
-        to learn things that I would not be able to normally learn through the university's course work.
-      </Typography>
       <div style={{ textAlign:"center" }}>
         <img src={model_image} align="center" alt="description" height="200"/>
-      </div>
+      <Typography>
+          This image showcases an image of a 3D model that I was able to find with an intentional void.
+      </Typography>
       <br/>
+      </div>
       <Typography variant="h6" color="textSecondary" paragraph>
         The research that I was helping conduct was finding data on 3D models that have hollow spaces, or voids, inside of them. The reason why we needed
         to find these 3D models was to be able to gather data on models that have intentional voids inside of them and determine that they are safe and intentional voids.
         This research is necessary because as 3D printing becomes more and more relevant, users would need a way to guarantee that the 3D model that they will be printing
         will be the model that they intend.
       </Typography>
+      <br/>
       <div style={{ textAlign:"center" }}>
         <img src={thingiverse_image} align="center" alt="description"/>
       </div>
@@ -34,9 +32,10 @@ const VSA = () =>{
         and managed to grasp a better understanding on how important a security signature on a file is.
       </Typography>
       <Typography variant="h6" color="textSecondary" paragraph>
-        The experience that I have gained through this research process is something that I could not learn from anywhere else. It has helped my resolve to 
-        delve into the world of cybersecurity for software. Through this research experience, I am helping to guarantee the security of a 3D model, and that is something
-        that I just find very interesting that this is something that I get to do.
+        With my python script, I used a python package called Selenium. What Selenium does is that it is able to mimic a web browser. This allows me to mimic a page on Thingiverse
+        and grab all of the files that appear at the head page. My python script then checks if that file has already been downloaded, and will skip it.
+        With the specific 3D model file type we are looking for is stl files, I would extract all of the stl files from each item in thingiverse. From there
+        I will run an analysis on all of those models, and from the analysis results. I am able to tell and determine which file has a void or hollow space inside of it.
       </Typography>
       <div style={{ textAlign:"center" }}>
       </div>
